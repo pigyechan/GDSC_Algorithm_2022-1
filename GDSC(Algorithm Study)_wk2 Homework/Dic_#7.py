e106eb43927e8e4c646a37a07c6ddf5bfb46b0a3
Yechan Lee <pigyechan@gmail.com>
2022-05-23 23:07:24 +0900

    GDSC(Algorithm Study)_wk2 Homework
    Dictionary

---
+++ GDSC(Algorithm Study)_wk2 Homework/Dic_#7.py
@@ -0,0 +1,9 @@
+icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+ice=list(icecream.values())
+sum = 0
+for i in ice:
+    sum += i
+
+print(sum)
+
+##print(sum(icecream.values()))
